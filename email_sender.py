c01b42a4f484b378c4eecbbfa6cf317ce59eb26c
Franky101 <becerra.frn@gmail.com>
2023-10-23 15:50:46 -0300

    Initial Commit

---
+++ email_sender.py
@@ -0,0 +1,31 @@
+
+from email.message import EmailMessage
+import getpass
+import os
+import ssl
+import smtplib
+
+
+email_sender = 'becerrafranco1992@gmail.com'
+# os.environ.get("EMAIL_PASSWORD")
+email_password = 'okkcoaisutyoykkp'
+# email_receiver = 'cimitax547@unbiex.com'
+email_receiver = 'becerrafranco1992@gmail.com'
+email_subject = 'Check out my SMPT code working with gmail'
+email_body = """
+    Hey, I am sending this email using Python, SMPT and Gmail.
+    """
+em = EmailMessage()
+em['Subject'] = email_subject
+em['From'] = email_sender
+em['To'] = email_receiver
+em.set_content(email_body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
+    server.login(email_sender, email_password)
+    server.send_message(em)
+    print('Email sent successfully')
+
+    
