8281561c3c7f98b4640674dc71be676cbac98d20
Franky101 <becerra.frn@gmail.com>
2023-11-07 19:07:45 -0300

    nofix: env broken again!

--- Emails/send_email.py
+++ Emails/send_email.py
@@ -7,15 +7,18 @@ from email.mime.multipart import MIMEMultipart # for multipart email (text and h
 dotenv.load_dotenv()
 
 # Environment variables
-username = os.environ.get("USERNAME")
-email_password = os.environ.get("PASSWORD")
-email_receiver = ['becerrafranco1992@gmail.com']
+#username = os.environ.get("USERNAME")
+#password = os.environ.get("PASSWORD")
+
+username = 'becerrafranco1992@gmail.com'
+password = 'okkcoaisutyoykkp'
+
+email_list = ['becerrafranco1992@gmail.com']
 email_subject = 'Check out my SMPT code working with gmail'
 
-print(username)
-print(email_password)
 
-def send_email(text='Email Body', subject=email_subject, from_email=f'Program Master <{username}>', to_emails=email_receiver, html=None):
+
+def send_email(text='Email Body', subject=email_subject, from_email='FrancoSender1 <becerrafranco1992@gmail.com>', to_emails=email_list, html=None):
     assert isinstance(to_emails, list)
     msg = MIMEMultipart('alternative')
     msg['From'] = from_email
@@ -27,13 +30,16 @@ def send_email(text='Email Body', subject=email_subject, from_email=f'Program Ma
     if html != None:
         html_part = MIMEText("<h1>This is working</h1>", 'html')
         msg.attach(html_part)
+
+    # Put message as string
     msg_str = msg.as_string()
 
     # Login to smpt server
     server = smtplib.SMTP(host='smtp.gmail.com', port=587)
     server.ehlo()
     server.starttls()
-    server.login(username, email_password)
+    server.login(username, password)
     server.sendmail(from_email, to_emails, msg_str)
     server.quit()
 
+send_email()
