a4c87564688f1c621815148541c3d3563f94ca20
Franky101 <becerra.frn@gmail.com>
2023-11-15 09:24:53 -0300

    feat: pulling inbox ready, no clear body format

--- read_Inbox/inbox.py
+++ read_Inbox/inbox.py
@@ -2,53 +2,55 @@ import imaplib
 import email
 import os
 import dotenv
-
-# inbox.py
-
 import sys
-
 
 # Add the parent directory of the current script to the sys.path
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(parent_dir)
 
-from email_send.send_email import send_email
-
-
+# from email_send.send_email import send_email
+# send_email()
 
 dotenv.load_dotenv()
-
-
 host = 'imap.gmail.com'
 username = os.environ.get("EMAIL_USER")
 password = os.environ.get("PASSWORD")
 
- 
-# Connect to host using SSL
-mail = imaplib.IMAP4_SSL(host)
-mail.login(username, password)
-mail.select("inbox")
+def get_inbox():
+    # Connect to host using SSL
+    mail = imaplib.IMAP4_SSL(host)
+    mail.login(username, password)
+    mail.select("inbox")
+    # Check Unseen messages
+    _, search_data = mail.search(None, 'UNSEEN')
+    my_message = []
+    for num in search_data[0].split():
+        email_data = {}
 
-# Check Unseen messages
-_, search_data = mail.search(None, 'UNSEEN')
+        _, data = mail.fetch(num, '(RFC822)')
+        _, b = data[0]
+        email_message = email.message_from_bytes(b)
 
+        for header in ['subject','to','from','date']:
+            print("{}: {}".format(header, email_message[header]))
+            email_data[header] = email_message[header]
 
+        for part in email_message.walk():
+            if part.get_content_type() == 'text/plain':
+                body = part.get_payload(decode=True)
+                email_data['body'] = body.decode()
 
-for num in search_data[0].split():
-    _, data = mail.fetch(num, '(RFC822)')
-    _, b = data[0]
-    email_message = email.message_from_bytes(b)
+            elif part.get_content_type() == 'text/html':
+                html_body = part.get_payload(decode=True)
+                print("HTML:\n")
+                email_data['html_body'] = html_body.decode()
+        my_message.append(email_data)
+    return my_message
 
-    for header in ['subject','to','from','date']:
-        print("{}: {}".format(header, email_message[header]))
-
-    for part in email_message.walk():
-        if part.get_content_type() == 'text/plain' or part.get_content_type() == 'text/html':
-            body = part.get_payload(decode=True)
-            print(body.decode())
-
-
+if __name__ == "__main__":
+    my_inbox = get_inbox()
+    print(my_inbox)
 
 
 
